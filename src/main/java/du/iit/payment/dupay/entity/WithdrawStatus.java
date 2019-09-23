745ebbe7b40d4d41ce067af02d8233ad06e8e215
Chinmoy007 <chinmoyacharjee15@gmail.com>
2019-09-23 19:32:59 +0600

    database class added, postgre integrated, entity and corresponding service and repo created and sample unit testing

---
+++ src/main/java/du/iit/payment/dupay/entity/WithdrawStatus.java
@@ -0,0 +1,7 @@
+package du.iit.payment.dupay.entity;
+
+public enum WithdrawStatus {
+    WITHDRAW_STATUS_A,
+    WITHDRAW_STATUS_B
+
+}
---
+++ src/main/java/du/iit/payment/dupay/entity/WithdrawWalletType.java
@@ -0,0 +1,6 @@
+package du.iit.payment.dupay.entity;
+
+public enum  WithdrawWalletType {
+    WITHDAWALLET_TYPE_A,
+    WITHDAWALLET_TYPE_B
+}
